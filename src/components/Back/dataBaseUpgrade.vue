06b011acbcaf30d52375da8cb04e28a889c5a9e4
lhj <2420479720@qq.com>
2019-07-08 15:26:15 +0800

    数据库升级页面修改

--- src/components/Back/dataBaseUpgrade.vue
+++ src/components/Back/dataBaseUpgrade.vue
@@ -54,6 +54,78 @@
 <script type="text/ecmascript-6">
 var info = [
   {
+    zoneId: '6b901f20-a621-4651-9fdf-cb6c63eb804a',
+    zoneName: '测试区域',
+    kernelList: [
+      {
+        label: '1核',
+        value: 1,
+        RAMList: [
+          { label: '1G', value: 1 },
+          { label: '2G', value: 2 },
+          { label: '4G', value: 4 },
+          { label: '8G', value: 8 }
+        ]
+      },
+      {
+        label: '2核',
+        value: 2,
+        RAMList: [
+          { label: '2G', value: 2 },
+          { label: '4G', value: 4 },
+          { label: '8G', value: 8 },
+          { label: '16G', value: 16 }
+        ]
+      },
+      {
+        label: '4核',
+        value: 4,
+        RAMList: [
+          { label: '4G', value: 4 },
+          { label: '8G', value: 8 },
+          { label: '16G', value: 16 },
+          { label: '32G', value: 32 }
+        ]
+      },
+      {
+        label: '8核',
+        value: 8,
+        RAMList: [
+          { label: '8G', value: 8 },
+          { label: '16G', value: 16 },
+          { label: '32G', value: 32 },
+          { label: '64G', value: 64 }
+        ]
+      },
+      {
+        label: '16核',
+        value: 16,
+        RAMList: [
+          { label: '16G', value: 16 },
+          { label: '32G', value: 32 },
+          { label: '64G', value: 64 },
+          { label: '128G', value: 128 }
+        ]
+      },
+      {
+        label: '32核',
+        value: 32,
+        RAMList: [
+          { label: '64G', value: 64 },
+          { label: '128G', value: 128 }
+        ]
+      },
+      {
+        label: '64核',
+        value: 64,
+        RAMList: [
+          { label: '128G', value: 128 },
+          { label: '256G', value: 256 },
+        ]
+      }
+    ],
+  },
+  {
     zoneId: '39a6af0b-6624-4194-b9d5-0c552d903858',
     zoneName: '北京一区(北京)',
     kernelList: [
