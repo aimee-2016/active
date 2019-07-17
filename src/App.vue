6c57a0ddf40db060f49362a28bbbde04e5a827c7
qrl <920149862@qq.com>
2019-07-17 18:55:41 +0800

    '717更新'

--- src/App.vue
+++ src/App.vue
@@ -494,7 +494,7 @@
                   {title: 'ECS快照', desc: '稳定可靠、安全保障', path: '/ecssnapshot/'},
                   {title: 'GPU服务器', desc: 'Tesla P100、Tesla P40 GPU', path: '/gpu/'},
                   {title: '弹性伸缩', desc: '高可用、可视化、低成本', path: '/elasticscalable/'},
-                  {title: 'DDoS高防主机', desc: '最大800G超大防护', path: '/ddos'},
+                  {title: 'DDoS高防主机（敬请期待）', desc: '最大800G超大防护', path: '/'},
                   {title: '裸金属服务器（敬请期待）', desc: '专属物理服务器', path: '/'}
                 ]
               },
