d9f079ba452d5c3ebb636746b41e0e55ab5b1ba1
eCheo <e1511404018@qq.com>
2019-08-14 16:54:48 +0800

    高防IP文档链接添加

--- src/components/Back/DDoSIP.vue
+++ src/components/Back/DDoSIP.vue
@@ -785,10 +785,10 @@
              </div>
             <div class="md-cer">
             <div style='display:flex;'>
-                <div style='margin-right:5px;'><a href='https://zschj.xrcloud.net/support_docs/1JWD4oQgr6_1LnzoqVYwB.html' target="_blank">源站IP/域名</a></div>
+                <div style='margin-right:5px;'><span>源站IP/域名</span></div>
                 <div>
                     <Input class="dp-area" type="textarea" v-model='sourceip' style='width:300px;display:inline-block;'></Input>
-                    <p class='dp-bf'>如果源站暴露，请参考使用 <span>高防后源站IP暴露的解决方法</span></p>
+                    <p class='dp-bf'>如果源站暴露，请参考使用 <a href='https://zschj.xrcloud.net/support_docs/1JWD4oQgr6_1LnzoqVYwB.html' target="_blank">高防后源站IP暴露的解决方法</a></p>
                 </div>
             </div>
             </div>
@@ -831,7 +831,7 @@
                     </FormItem>
                     <FormItem label="源站IP/域名" prop="ip">
                         <Input class="dp-area" type="textarea" v-model="addDomainList.ip" placeholder="多个域名与IP用；隔开"></Input>
-                        <p class='dp-bf'>如果源站暴露，请参考使用 <span>高防后源站IP暴露的解决方法</span></p>
+                        <p class='dp-bf'>如果源站暴露，请参考使用 <a href='https://zschj.xrcloud.net/support_docs/1JWD4oQgr6_1LnzoqVYwB.html' target="_blank">高防后源站IP暴露的解决方法</a></p>
                     </FormItem>
                  </Form>   
             </div>
