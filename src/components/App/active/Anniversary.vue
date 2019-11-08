d6316a1dbc83465161c979f15b2aba6a82973c63
lhj <2420479720@qq.com>
2019-11-08 13:57:29 +0800

    禅道bug

--- src/components/App/active/Anniversary.vue
+++ src/components/App/active/Anniversary.vue
@@ -380,6 +380,7 @@
       class="aa-modal rule"
       title="活动规则"
       :mask-closable="false"
+      :scrollable="true"
     >
       <div class="content">
         <ul>
@@ -396,6 +397,7 @@
       class="aa-modal rule"
       title="活动规则"
       :mask-closable="false"
+      :scrollable="true"
     >
       <div class="content">
         <ul>
@@ -412,6 +414,7 @@
       class="aa-modal rule"
       title="活动规则"
       :mask-closable="false"
+      :scrollable="true"
     >
       <div class="content">
         <ul>
@@ -428,6 +431,7 @@
       class="aa-modal rule"
       title="活动规则"
       :mask-closable="false"
+      :scrollable="true"
     >
       <div class="content">
         <ul>
@@ -561,7 +565,7 @@
         </p>
       </div>
       <div slot="footer" class="modal-footer-border">
-        <Button type="primary" @click="showModal.qrCode = false">确定</Button>
+        <Button type="primary" @click="qrcodeClose">确定</Button>
       </div>
     </Modal>
   </div>
@@ -1142,7 +1146,9 @@ export default {
           this.qrConfig.value = this.shareUrl
           this.showModal.share = true
         } else {
-          this.$Message.info("平台出小差了");
+          this.$message.info({
+            content: response.data.message
+          })
         }
       })
     },
@@ -1303,6 +1309,10 @@ export default {
           }
         })
       }, 3000)
+    },
+    qrcodeClose() {
+      this.showModal.qrCode = false
+      clearInterval(this.codeTimer)
     },
     Callpresentation () {
       this.$refs.cashverification.validateField('messagecode', (text) => {
@@ -2098,8 +2108,9 @@ section:nth-of-type(4) {
   }
 }
 .share-modal {
-  p {
+  >p {
     font-size: 16px;
+    line-height: 1.5;
     text-align: center;
   }
   .wrapper {
@@ -2429,5 +2440,26 @@ section:nth-of-type(4) {
       font-size: 16px;
     }
   }
+  .share-modal {
+  .wrapper {
+    margin-top: 30px;
+    text-align: center;
+    div {
+      display: inline-block;
+      span {
+        margin-top: 0px;
+      }
+    }
+  }
+  .qr-code {
+    margin-right: 0px;
+  }
+  .url {
+    .btn {
+      margin-bottom: 15px;
+      margin-top: 20px;
+    }
+  }
+}
 }
 </style>
